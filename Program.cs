92cfa9293a1820a102b5f7498bf5491d3e3c4d53
Bournane Mohamed <mbournane@gmail.com>
2023-05-31 10:44:03 +0200

    first array

--- Program.cs
+++ Program.cs
@@ -12,16 +12,8 @@ class Program
         firstArray = new int[] {0,1,2,3,4};
         secondArray = new int[] {5,6,7,8,9};
         
-        thirdArray[0]= firstArray[0];
-        thirdArray[1]= firstArray[1];
-        thirdArray[2]= firstArray[2];
-        thirdArray[3]= firstArray[3];
-        thirdArray[4]= firstArray[4];
-        thirdArray[5]= secondArray[0];
-        thirdArray[6]= secondArray[1];
-        thirdArray[7]= secondArray[2];
-        thirdArray[8]= secondArray[3];
-        thirdArray[9]= secondArray[4];
+        firstArray.CopyTo(thirdArray, 0);
+        secondArray.CopyTo(thirdArray, firstArray.Length);
 
         Console.WriteLine("Valeurs du premier tableau");
         for (int i = 0; i< firstArray.Length; i++)
